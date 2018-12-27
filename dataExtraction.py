9b33087007e0d32dd82ffcaf317f8eed5b9aed36
Arnav Chavan <44640932+Arnav0400@users.noreply.github.com>
2018-12-27 13:45:54 +0530

    Update dataExtraction.py

--- dataExtraction.py
+++ dataExtraction.py
@@ -8,10 +8,10 @@ import tweepy
 import csv
  
 #credentials
-consumerKey = "gIUKz7MW2kbE7zNO97mq4yvi7"
-consumerSecret = "rNV0kZQ8NzIiQkyUEMZzEM8bzatx1Iv9mKy5Ib9O30HFyZbdeA"
-accessToken = "311381463-86fWlo6vntrEaDXKMROFTPeoxi6hnrodJiJ3SXUC"
-accessTokenSecret = "qI6CvCeiP6BP2isgorfCgMoJojKKnR82U3pWMoZidww7e"
+consumerKey = ""
+consumerSecret = ""
+accessToken = ""
+accessTokenSecret = ""
 
 #Function to extract tweets
 def get_tweets(no_of_tweets):
